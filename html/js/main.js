a4a17d21b03c796e97526e902c61ba88d5210217
David Denker <david@denker-interaktiv.de>
2017-01-09 18:37:40 +0000

    Changes

--- html/js/main.js
+++ html/js/main.js
@@ -1,10 +1,68 @@
 var database;
-$(function() {
-  // Handler for .ready() called.
-  console.log("Hello");
-  init_interface();
+var wdProtocol = "echo-protocol"
+var wsUri = "ws://192.168.178.53:8080/";
+var output;
 
+$(function() {
+  init_interface();
+  init_websocket();
 });
+
+
+  function init_websocket()
+  {
+    output = document.getElementById("console_output");
+    testWebSocket();
+  }
+
+  function testWebSocket()
+  {
+    websocket = new WebSocket(wsUri,wdProtocol);
+    websocket.onopen = function(evt) { onOpen(evt) };
+    websocket.onclose = function(evt) { onClose(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    websocket.onerror = function(evt) { onError(evt) };
+  }
+
+  function onOpen(evt)
+  {
+    writeToScreen("CONNECTED");
+    doSend("WebSocket rocks");
+  }
+
+  function onClose(evt)
+  {
+    writeToScreen("DISCONNECTED");
+  }
+
+  function onMessage(evt)
+  {
+    writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>');
+    websocket.close();
+  }
+
+  function onError(evt)
+  {
+    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+  }
+
+  function doSend(message)
+  {
+    writeToScreen("SENT: " + message);
+    websocket.send(message);
+  }
+
+  function writeToScreen(message)
+  {
+    var pre = document.createElement("p");
+    pre.style.wordWrap = "break-word";
+    pre.innerHTML = message;
+    output.appendChild(pre);
+  }
+
+
+
+
 
 function init_interface()
 {
